33f2edfcc71c4f60e795fe256a8c9affd1108717
hirajya <rodneyestrada2425@gmail.com>
2024-04-28 11:46:11 +0800

    done structure

---
+++ src/controller/mainController.java
@@ -0,0 +1,84 @@
+package controller;
+
+import java.io.IOError;
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+public class mainController {
+    
+
+    @FXML
+    Button order_btn, analytics_btn, pending_orders_btn, done_orders_btn;
+
+    @FXML
+    BorderPane mainPane;
+
+    @FXML
+    AnchorPane subPane;
+
+
+    @FXML
+    public void initialize() throws IOException {
+        loadView("/view/order.fxml");
+    }
+
+    public void handleOrderButton() throws IOException{
+        order_btn.setOnAction(e -> {
+            System.out.println("Order Button Clicked");
+            try {
+                loadView("/view/order.fxml");
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+        });
+    }
+
+    public void handleAnalyticsButton() throws IOException{
+        analytics_btn.setOnAction(e -> {
+            System.out.println("Analytics Button Clicked");
+            try {
+                loadView("/view/analytics.fxml");
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+        });
+    }
+
+    public void handleDoneOrdersButton() throws IOException{
+        done_orders_btn.setOnAction(e -> {
+            System.out.println("Done Orders Button Clicked");
+            try {
+                loadView("/view/done_orders.fxml");
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+        });
+    }
+
+    public void handlePendingOrdersButton() throws IOException{
+        pending_orders_btn.setOnAction(e -> {
+            System.out.println("Pending Orders Button Clicked");
+            try {
+                loadView("/view/pending_orders.fxml");
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+        });
+    }
+
+    private void loadView(String fxmlFile) throws IOException {
+    AnchorPane newView = FXMLLoader.load(getClass().getResource(fxmlFile));
+    ((AnchorPane) mainPane.getCenter()).getChildren().clear();  // Clear existing content
+    ((AnchorPane) mainPane.getCenter()).getChildren().add(newView);  // Add new view to center AnchorPane
+}
+
+}
